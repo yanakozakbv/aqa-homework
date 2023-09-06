fba20ffcd475b3405018686cee43acf0316ed58e
Yana Kozak <yana.kozak@blackthorn-vision.com>
2023-09-06 23:07:53 +0300

    domashka1

---
+++ Domashka/Domashka1/Domashka1.py
@@ -0,0 +1,35 @@
+#1
+john_salary = 5400.5
+marta_salary = 5000.5
+
+print(john_salary,',', marta_salary)
+
+#2
+john_age = int(27)
+marta_age = int(19)
+
+print(john_age,',', marta_age)
+
+#3
+john_name = 'John'
+marta_name = 'Marta'
+
+print(john_name,',', marta_name)
+
+#4
+john_gender = False
+marta_gender = True
+
+print(john_gender,',', marta_gender)
+
+#5
+john_friends = ["Max", "David", "Joe", "Max"]
+marta_friends = ["Ann", "Mary", "Sarah"]
+
+print(john_friends)
+print(marta_friends)
+
+#6
+people = ["Max", "David", "Joe", "Max", "Mary", "Ann", "Mary"]
+people_set = set(people)
+print(people_set)
